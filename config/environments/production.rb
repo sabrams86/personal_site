812fb1a6b0a27a2f94d5e077769e41caba9c8970
Steven Abrams <steven.abrams86@gmail.com>
2016-10-11 20:46:00 -0600

    remove extra settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,15 +24,12 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # config.serve_static_assets = false
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
-  config.assets.compile = false
-
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
