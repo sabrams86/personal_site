13496553f9ba036d88eb7d19e332bbc11ca4e142
Steven Abrams <steven.abrams86@gmail.com>
2016-10-11 20:42:34 -0600

    more assets trying

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,13 +24,13 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  config.serve_static_assets = true
+  config.serve_static_assets = false
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
 
