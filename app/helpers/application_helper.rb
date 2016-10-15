6158f31359bcd802522b81761cad13097675b6ab
Steven Abrams <steven.abrams86@gmail.com>
2016-10-15 11:04:50 -0600

    add some css for posts and add red carpet options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,13 +6,20 @@ module ApplicationHelper
       hard_wrap:       true,
       link_attributes: { rel: 'nofollow', target: "_blank" },
       space_after_headers: true,
-      fenced_code_blocks: true
+      fenced_code_blocks: true,
+      prettify: true
     }
 
     extensions = {
       autolink:           true,
       superscript:        true,
-      disable_indented_code_blocks: true
+      disable_indented_code_blocks: true,
+      underline: true,
+      highlight: true,
+      superscript: true,
+      strikethrough: true,
+      no_intra_emphasis: true,
+      tables: true
     }
 
     renderer = Redcarpet::Render::HTML.new(options)
