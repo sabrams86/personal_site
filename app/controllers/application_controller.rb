ccb84ead49aeab6f9440f4de9d84b274537cac55
Steven Abrams <steven.abrams86@gmail.com>
2016-10-15 18:09:07 -0600

    add devise redirect to previous page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :store_current_location, :unless => :devise_controller?
 
   private
+  # override the devise helper to store the current location so we can
+  # redirect to it after loggin in or out. This override makes signing in
+  # and signing up work automatically.
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+  
+  # override the devise method for where to go after signing out because theirs
+  # always goes to the root path. Because devise uses a session variable and
+  # the session is destroyed on log out, we need to use request.referrer
+  # root_path is there as a backup
+  def after_sign_out_path_for(resource)
+    request.referrer || root_path
+  end
 
   def authenticate_admin
     unless current_user.try(:admin?)
